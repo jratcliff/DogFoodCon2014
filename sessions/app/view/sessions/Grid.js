6263f4993d040679ab1a46fa8bacdfd152ee226b
Jack Ratcliff <jratcliff@sencha.com>
2014-10-13 10:26:34 -0500

    Step 3 - Give new Grid an xtype of 'sessions-grid' and add a columns config defining columns for data that describes a conference session

--- sessions/app/view/sessions/Grid.js
+++ sessions/app/view/sessions/Grid.js
@@ -1,11 +1,33 @@
 
 Ext.define("DogFood.view.sessions.Grid",{
     extend: "Ext.grid.Panel",
+    xtype : 'sessions-grid',
 
     controller: "sessions-grid",
     viewModel: {
         type: "sessions-grid"
     },
 
-    html: "Hello, World!!"
+    columns: [
+        {
+            header      : 'Track',
+            flex        : 1
+        },
+        {
+            header      : 'Level',
+            width       : 100
+        },
+        {
+            header      : 'Title',
+            flex        : 1
+        },
+        {
+            header      : 'Speaker',
+            flex        : 1
+        },
+        {
+            header      : 'Description',
+            flex        : 2
+        }
+    ]
 });
