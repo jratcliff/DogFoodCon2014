4599be0d51633546c05851524018d7adf41eed37
Jack Ratcliff <jratcliff@sencha.com>
2014-10-13 10:49:37 -0500

    Step 6 - Update the columns on the Grid with the appropriate dataIndex that matches the name of the field defined on the model.

--- sessions/app/view/sessions/Grid.js
+++ sessions/app/view/sessions/Grid.js
@@ -3,6 +3,11 @@ Ext.define("DogFood.view.sessions.Grid",{
     extend: "Ext.grid.Panel",
     xtype : 'sessions-grid',
 
+    requires: [
+        'DogFood.view.sessions.GridModel',
+        'DogFood.view.sessions.GridController'
+    ],
+
     controller: "sessions-grid",
     viewModel: {
         type: "sessions-grid"
@@ -11,22 +16,27 @@ Ext.define("DogFood.view.sessions.Grid",{
     columns: [
         {
             header      : 'Track',
+            dataIndex   : 'track',
             flex        : 1
         },
         {
             header      : 'Level',
+            dataIndex   : 'level',
             width       : 100
         },
         {
             header      : 'Title',
+            dataIndex   : 'title',
             flex        : 1
         },
         {
             header      : 'Speaker',
+            dataIndex   : 'speaker',
             flex        : 1
         },
         {
             header      : 'Description',
+            dataIndex   : 'description',
             flex        : 2
         }
     ]
